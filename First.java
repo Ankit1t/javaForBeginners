5361ef6ffbee8a36dcca998aa2aec577ebd07f4a
Anki!t <ankitmaurya1869@gmail.com>
2025-11-18 07:46:14 +0000

    Add First.java file

---
+++ First.java
@@ -0,0 +1,7 @@
+package JavaForBeginners.Lesson_34_Exceptions;
+
+public class First {
+    public static void main(String[] args) {
+      System.out.println("Hello guys....!\n Welcome to the Real world of Java programming....!");
+    }
+}
